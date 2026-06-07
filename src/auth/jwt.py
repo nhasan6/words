6e5a323dbcfe062e4a3419939bebf9f0242a1587
Neeya <169307919+nhasan6@users.noreply.github.com>
2026-06-07 17:44:15 -0400

    fix var name

--- src/auth/jwt.py
+++ src/auth/jwt.py
@@ -13,5 +13,5 @@ def create_access_token(data: dict, expires_delta: timedelta | None = None):
     else:
         expire = datetime.now(timezone.utc) + timedelta(minutes=15)
     to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=ALGORITHM)
+    encoded_jwt = jwt.encode(to_encode, settings.SECRET_JWT_KEY, algorithm=ALGORITHM)
     return encoded_jwt

603241b69ae2767df8038382e0e055603b8d3a69
Neeya <169307919+nhasan6@users.noreply.github.com>
2026-07-09 11:23:12 -0400

    update user schemas

--- src/schemas/user.py
+++ src/schemas/user.py
@@ -11,5 +11,7 @@ class UserCreate(UserBase):
 class UserResponse(UserBase):
     id: int
     created_at: datetime
-    class Config: from_attributes = True
+    model_config = {
+        "from_attributes": True
+    }
 
